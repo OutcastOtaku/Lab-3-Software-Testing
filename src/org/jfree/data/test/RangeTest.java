78c093a1a0d7f3efd9827bf7e8efb056cba83abf
Oliver Looney <88001922+Oliver-Looney@users.noreply.github.com>
2023-03-22 17:18:29 +0000

    line coverage: 94%, branch coverage: 86%

--- src/org/jfree/data/test/RangeTest.java
+++ src/org/jfree/data/test/RangeTest.java
@@ -282,5 +282,338 @@ public class RangeTest {
         double actual = range.getCentralValue();
         assertTrue(Double.isNaN(actual));
     }
-    
+	
+	
+    @Test
+    public void testCombine_NullRanges() {
+        Range result = Range.combine(null, null);
+        assertNull(result);
+    }
+
+    @Test
+    public void testCombine_NullRange1() {
+        Range range2 = new Range(1.0, 2.0);
+        Range result = Range.combine(null, range2);
+        assertEquals(range2, result);
+    }
+
+    @Test
+    public void testCombine_NullRange2() {
+        Range range1 = new Range(1.0, 2.0);
+        Range result = Range.combine(range1, null);
+        assertEquals(range1, result);
+    }
+
+    @Test
+    public void testCombine_BothRanges() {
+        Range range1 = new Range(1.0, 2.0);
+        Range range2 = new Range(3.0, 4.0);
+        Range result = Range.combine(range1, range2);
+        Range expected = new Range(1.0, 4.0);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testCombine_Range1LargerThanRange2() {
+        Range range1 = new Range(3.0, 4.0);
+        Range range2 = new Range(1.0, 2.0);
+        Range result = Range.combine(range1, range2);
+        Range expected = new Range(1.0, 4.0);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testCombine_Range2LargerThanRange1() {
+        Range range1 = new Range(1.0, 3.0);
+        Range range2 = new Range(2.0, 4.0);
+        Range result = Range.combine(range1, range2);
+        Range expected = new Range(1.0, 4.0);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testCombine_RangesOverlap() {
+        Range range1 = new Range(1.0, 2.0);
+        Range range2 = new Range(1.5, 3.0);
+        Range result = Range.combine(range1, range2);
+        Range expected = new Range(1.0, 3.0);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testCombine_Range1LowerThanRange2() {
+        Range range1 = new Range(1.0, 2.0);
+        Range range2 = new Range(0.0, 1.5);
+        Range result = Range.combine(range1, range2);
+        Range expected = new Range(0.0, 2.0);
+        assertEquals(expected, result);
+    }
+    @Test
+    public void testExpandToInclude_NullRange() {
+        Range range = null;
+        double value = 5.0;
+        Range expected = new Range(value, value);
+        Range actual = Range.expandToInclude(range, value);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandToInclude_ValueGreaterThanRange() {
+        Range range = new Range(2.0, 6.0);
+        double value = 8.0;
+        Range expected = new Range(2.0, 8.0);
+        Range actual = Range.expandToInclude(range, value);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandToInclude_ValueInsideRange() {
+        Range range = new Range(2.0, 6.0);
+        double value = 3.0;
+        Range expected = new Range(2.0, 6.0);
+        Range actual = Range.expandToInclude(range, value);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandToInclude_ValueOnLowerBound() {
+        Range range = new Range(2.0, 6.0);
+        double value = 2.0;
+        Range expected = new Range(2.0, 6.0);
+        Range actual = Range.expandToInclude(range, value);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testExpandToInclude_ValueOnUpperBound() {
+        Range range = new Range(2.0, 6.0);
+        Range expandedRange = Range.expandToInclude(range, 6.0);
+        Assert.assertEquals("Unexpected expanded range", range, expandedRange);
+    }
+
+    @Test
+    public void testExpandToInclude_ValueBelowLowerBound() {
+        Range range = new Range(2.0, 6.0);
+        Range expandedRange = Range.expandToInclude(range, -2.0);
+        Range expectedRange = new Range(-2.0, 6.0);
+        Assert.assertEquals("Unexpected expanded range", expectedRange, expandedRange);
+    }
+
+    @Test
+    public void testExpandToInclude_ValueAboveUpperBound() {
+        Range range = new Range(2.0, 6.0);
+        Range expandedRange = Range.expandToInclude(range, 10.0);
+        Range expectedRange = new Range(2.0, 10.0);
+        Assert.assertEquals("Unexpected expanded range", expectedRange, expandedRange);
+    }
+    @Test
+    public void testExpandWithMargins() {
+        Range range = new Range(2, 6);
+        double lowerMargin = 0.25;
+        double upperMargin = 0.5;
+        Range expected = new Range(1, 8);
+        Range actual = Range.expand(range, lowerMargin, upperMargin);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testExpandWithEqualMargins() {
+        Range range = new Range(0, 10);
+        double margin = 0.5;
+        Range expected = new Range(-5, 15);
+        Range actual = Range.expand(range, margin, margin);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testExpandWithZeroMargins() {
+        Range range = new Range(3, 7);
+        double lowerMargin = 0;
+        double upperMargin = 0;
+        Range expected = new Range(3, 7);
+        Range actual = Range.expand(range, lowerMargin, upperMargin);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testExpandWithLargeMargins() {
+        Range range = new Range(-5, 5);
+        double margin = 0.75;
+        Range expected = new Range(-15.0, 15.0);
+        Range actual = Range.expand(range, margin, margin);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testExpandRangeWithMargins() {
+        Range originalRange = new Range(0.5, 3.5);
+        double lowerMargin = 0.1;
+        double upperMargin = 0.2;
+        Range expectedRange = new Range(0.05, 3.95);
+
+        Range expandedRange = Range.expand(originalRange, lowerMargin, upperMargin);
+
+        assertEquals(expectedRange, expandedRange);
+    }
+
+    @Test
+    public void testExpandZeroLengthRange() {
+        Range originalRange = new Range(0.0, 0.0);
+        double lowerMargin = 0.1;
+        double upperMargin = 0.2;
+        Range expectedRange = new Range(0.0, 0.0);
+
+        Range expandedRange = Range.expand(originalRange, lowerMargin, upperMargin);
+
+        assertEquals(expectedRange, expandedRange);
+    }
+
+    @Test
+    public void testExpandInfiniteRange() {
+        Range originalRange = new Range(Double.NEGATIVE_INFINITY, Double.MAX_VALUE);
+        double lowerMargin = 0.1;
+        double upperMargin = 0.2;
+        Range expectedRange = new Range(Double.NEGATIVE_INFINITY, Double.MAX_VALUE);
+
+        Range expandedRange = Range.expand(originalRange, lowerMargin, upperMargin);
+
+        assertEquals(expectedRange, expandedRange);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testExpandNullRange() {
+        Range originalRange = null;
+        double lowerMargin = 0.25;
+        double upperMargin = 0.5;
+
+        Range expandedRange = Range.expand(originalRange, lowerMargin, upperMargin);
+
+        // exception should be thrown before reaching this point
+        fail("Expected IllegalArgumentException but no exception was thrown.");
+    }
+
+    @Test
+    public void testHashCodeForRangeWithSameLowerAndUpper() {
+        Range range = new Range(0.0, 0.0);
+        int expected = 31;
+        int actual = range.hashCode();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testHashCodeForRangeWithDifferentLowerAndUpper() {
+        Range range = new Range(1.0, 2.0);
+        int expected = -535610400;
+        int actual = range.hashCode();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testHashCodeForRangeWithNegativeLowerAndUpper() {
+        Range range = new Range(-1.0, -2.0);
+        int expected = 1894476323;
+        int actual = range.hashCode();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testHashCodeWithNaN() {
+        Range range = new Range(Double.NaN, Double.NaN);
+        int expectedHashCode = 961;
+        int actualHashCode = range.hashCode();
+        assertEquals(expectedHashCode, actualHashCode);
+    }
+
+    @Test
+    public void testHashCodeWithPositiveInfinity() {
+        Range range = new Range(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+        int expectedHashCode = 2146959367;
+        int actualHashCode = range.hashCode();
+        assertEquals(expectedHashCode, actualHashCode);
+    }
+
+    @Test
+    public void testHashCodeWithNegativeInfinity() {
+        Range range = new Range(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
+        int expectedHashCode = -1048575;
+        int actualHashCode = range.hashCode();
+        assertEquals(expectedHashCode, actualHashCode);
+    }
+
+    @Test
+    public void testShiftWithPositiveDeltaAndZeroCrossingAllowed1() {
+        Range base = new Range(0, 10);
+        double delta = 5;
+        boolean allowZeroCrossing = true;
+
+        Range expected = new Range(5, 15);
+        Range actual = Range.shift(base, delta, allowZeroCrossing);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testShiftWithPositiveDeltaAndZeroCrossingNotAllowed() {
+        Range base = new Range(-5, 5);
+        double delta = 2;
+        boolean allowZeroCrossing = true;
+
+        Range expected = new Range(-3, 7);
+        Range actual = Range.shift(base, delta, allowZeroCrossing);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testShiftWithPositiveDeltaAndZeroCrossingAllowed() {
+        Range base = new Range(-3, 3);
+        double delta = 4;
+        boolean allowZeroCrossing = true;
+
+        Range expected = new Range(1, 7);
+        Range actual = Range.shift(base, delta, allowZeroCrossing);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testShiftWithNegativeDeltaAndZeroCrossingAllowed() {
+        Range base = new Range(-2, 4);
+        double delta = -3;
+        boolean allowZeroCrossing = true;
+
+        Range expected = new Range(-5, 1);
+        Range actual = Range.shift(base, delta, allowZeroCrossing);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testShiftCase5() {
+        Range base = new Range(0, 10);
+        Range shifted = Range.shift(base, 5, false);
+        Range expected = new Range(5, 10);
+        assertEquals(expected, shifted);
+    }
+
+    @Test
+    public void testShiftCase6() {
+        Range base = new Range(-5, 5);
+        Range shifted = Range.shift(base, 2, false);
+        Range expected = new Range(-3, 5);
+        assertEquals(expected, shifted);
+    }
+
+    @Test
+    public void testShiftCase7() {
+        Range base = new Range(-3, 3);
+        Range shifted = Range.shift(base, 4, false);
+        Range expected = new Range(0, 7);
+        assertEquals(expected, shifted);
+    }
+
+    @Test
+    public void testShiftCase8() {
+        Range base = new Range(-2, 4);
+        Range shifted = Range.shift(base, -3, false);
+        Range expected = new Range(-5, 1);
+        assertEquals(expected, shifted);
+    }
+}
+ 	   
 }
